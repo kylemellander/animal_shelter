efd647610a83ce07dd1158ee96f7cf2abf2baad2
Rachael Berecka & Kyle Mellander <student@epicodus.com>
2015-08-18 12:11:54 -0700

    Add test for deleting single animal

--- spec/shelter_spec.rb
+++ spec/shelter_spec.rb
@@ -20,7 +20,12 @@ describe(Animal) do
       test_animal = Animal.new({:name => "Roo", :type => "Kangaroo", :breed => "Hoppy", :gender => "M", :date => "2015-08-18 00:00:00", :pic => "http://vignette1.wikia.nocookie.net/winniethepooh/images/2/2a/Roo.PNG/revision/latest?cb=20081015165029"})
       test_animal.save()
       Animal.delete()
-      expect(Animal.all()).to(eq([]))      
+      expect(Animal.all()).to(eq([]))
+      test_animal.save()
+      test_animal2 = Animal.new({:name => "Winnie the Pooh", :type => "Bear", :breed => "Stuffed", :gender => "M", :date => "2015-08-17 00:00:00", :pic => "http://www.cartoonpics.net/data/media/33/gallery_poster_winnie_pooh.jpg"})
+      test_animal2.save()
+      Animal.delete(test_animal.id())
+      expect(Animal.all()).to(eq([test_animal2]))      
     end
   end
 end
